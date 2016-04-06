86c9da8438cbeeaa0ef99ebfd1360b8e82f10235
Nate Brochin <nbrochin@gmail.com>
2016-04-06 01:35:03 -0400

    logic and styles

--- js/app.js
+++ js/app.js
@@ -1,17 +1,83 @@
 
+ function genRandnum(){
+
+	 return Math.floor(Math.random()* 100 ) + 1;
+	}
+	var randNum = genRandnum();
+	var counter = 1;
+
+
 $(document).ready(function(){
-	
+
+	$('#guessButton').click(function(){
+			event.preventDefault();
+
+    //no empty fields
+		var guess = $('#userGuess').val();
+		guess = parseInt(guess);
+		console.log('Secret Random Number is ' + randNum +'.');
+
+        //add guesses to list
+			if( 1 <= guess && guess <= 100 ) {
+				$('#guessList').append("<li>"+guess+"</li>");
+				console.log(guess);
+        // viewing number of guesses
+				$('#count').html(counter++);
+				var spread =  Math.abs(randNum - guess);
+				
+				// hot and cold logic
+				console.log(spread);
+				if(spread >=60){
+					$('#feedback').replaceWith("<h2 id='feedback'>polar tundra, ninny.</h2>");
+				}
+				else if(spread >= 40){
+					$('#feedback').replaceWith("<h2 id='feedback'>january in Milwaulkee. my face hurts.</h2>");
+				}
+				else if(spread >=25){
+					$('#feedback').replaceWith("<h2 id='feedback'>march in London. little chilly.</h2>");
+				}
+				else if(spread >=17){
+					$('#feedback').replaceWith("<h2 id='feedback'>may in Boston. nice breeze.</h2>");
+				}
+				else if(spread >=10){
+					$('#feedback').replaceWith("<h2 id='feedback'>june in Miami. litte uncomfortable.</h2>");
+				}
+				else if(spread >=3){
+					$('#feedback').replaceWith("<h2 id='feedback'>july in Baton Rouge. heat stroke.</h2>");
+				}
+				else if(spread === 0){
+					$('#feedback').replaceWith("<h2 id='feedback'>winner. winner. chicken dinner!!</h2>");
+				}
+
+			}
+			else{
+				alert('Please enter a number between 1 and 100');
+			}
+		});
+
+		$('.new').click(function(){
+		$('h2#feedback').replaceWith("<h2 id='feedback'>pick a number, any number (betwixt 1 and 100).</h2");
+		$('#guessList li').remove();
+		$('#userGuess').val('');
+		$('#count').replaceWith("<span id='count'>0</span>");
+
+    var counter = 1;
+
+		//generate random number, again; otherwise it get's stuck
+		var randNum = genRandnum();
+		console.log('new number that will be replaced ' + randNum);
+
+	});
+
 	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+ 	$(".what").click(function(){
+   	$(".overlay").fadeIn(1000);
 
   	});
 
   	/*--- Hide information modal box ---*/
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
-  	});
+	});
 
 });
-
-
